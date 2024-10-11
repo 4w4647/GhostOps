5989cb0ea3b17465ab76278cf5235fe3951e79cf
Awagat Dhungana <4w4647@gmail.com>
2024-10-11 10:40:44 +0545

    Progress

--- web/public/js/panel.js
+++ web/public/js/panel.js
@@ -1,15 +1,61 @@
 class Panel {
     constructor() {
-        this.body = document.body;
+        this.container = document.getElementById("container");
+        this.navigationButtons = document.querySelectorAll("ul li");
+        this.pages = {
+            dashboard: {
+                html: `<h1>Dashboard</h1>`,
+                script: ``
+            },
+            agents: {
+                html: `<h1>Agents</h1>`,
+                script: ``
+            },
+            listeners: {
+                html: `<h1>Listeners</h1>`,
+                script: ``
+            },
+            worldmap: {
+                html: `<h1>World Map</h1>`,
+                script: ``
+            },
+            database: {
+                html: `<h1>Database</h1>`,
+                script: ``
+            },
+            notifications: {
+                html: `<h1>Notifications</h1>`,
+                script: ``
+            },
+            settings: {
+                html: `<h1>Settings</h1>`,
+                script: ``
+            },
+            logout: {
+                html: `<h1>Logout</h1>`,
+                script: ``
+            }
+        };
         this.init();
     }
 
-    createNavigationBar() {
-        this.navigationBar = document.createElement("nav");
+    changePage(button) {
+        this.navigationButtons.forEach(button => {
+            button.classList.remove("active");
+        });
+        button.classList.add("active");
+        this.container.innerHTML = this.pages[button.id]["html"];
+        eval(this.pages[button.id]["script"]);
     }
 
     init() {
+        this.changePage(document.getElementById("dashboard"));
 
+        this.navigationButtons.forEach(button => {
+            button.addEventListener("click", () => {
+                this.changePage(button);
+            });
+        });
     }
 }
 
